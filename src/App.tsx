4bc27cee4db25ed7802148a2e5a17e6313f78f4c
Christopher Goh <chrisgzf@gmail.com>
2021-05-17 19:34:33 +0800

    fix: 100vh on mobile including browser bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import PageAudioCall from "./pages/PageAudioCall";
 import PageChannelForm from "./pages/PageChannelForm";
 
 function App() {
+  // Hack to use 100vh without including browser bar on mobile
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
+
   return (
     <Router>
       <Switch>
